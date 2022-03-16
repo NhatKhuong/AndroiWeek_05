b01bac080620631bde3aaa749f1a64454bd52557
DangNhatKhuong <nhatkhuong2001@gmail.com>
2022-03-16 12:54:20 +0700

    'lab5ver5'

--- app/src/main/java/com/example/androi_week05/CustomGoodApdapter.java
+++ app/src/main/java/com/example/androi_week05/CustomGoodApdapter.java
@@ -2,34 +2,47 @@ package com.example.androi_week05;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class CustomGoodApdapter extends BaseAdapter {
 
-    Context ctx;
-    int layoutItem;
-    ArrayList<Goods> arrayList;
+    private Context ctx;
+    private int layoutItem;
+    private ArrayList<Goods> arrayList;
+    private List<Goods> listFilter;
+    private int positionSelect = -1;
 
 
     public CustomGoodApdapter(Context ctx, int layoutItem, ArrayList<Goods> arrayList) {
         this.ctx = ctx;
         this.layoutItem = layoutItem;
         this.arrayList = arrayList;
+        this.listFilter = arrayList;
+        System.out.println("list view" + listFilter);
     }
 
     @Override
     public int getCount() {
-        return arrayList.size();
+        if(listFilter.size() !=0 && !listFilter.isEmpty()){
+            return  listFilter.size();
+        }
+        return  0;
     }
 
     @Override
@@ -43,38 +56,85 @@ public class CustomGoodApdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        view = LayoutInflater.from(ctx).inflate(layoutItem, viewGroup,false);
+    public View getView(int i, View convetView, ViewGroup viewGroup) {
+        convetView = LayoutInflater.from(ctx).inflate(layoutItem, viewGroup,false);
 
-        TextView txtName =view.findViewById(R.id.txtname);
-        TextView txtdescription =view.findViewById(R.id.txtdesciption);
-        TextView txtprice =view.findViewById(R.id.txtprice);
-        ImageView imageitem =view.findViewById(R.id.imageitem);
+        TextView txtName = (TextView) convetView.findViewById(R.id.txtname);
+        TextView txtdescription = (TextView)convetView.findViewById(R.id.txtdesciption);
+        TextView txtprice =(TextView) convetView.findViewById(R.id.txtprice);
+        ImageView imageitem = (ImageView) convetView.findViewById(R.id.imageitem);
 
-        Button btnD = view.findViewById(R.id.btnDount);
-        Button btnP = view.findViewById(R.id.btnPink);
-        Button btnF = view.findViewById(R.id.btnFloat);
+        View viewItem = convetView.findViewById(R.id.view);
 
-
-
-        txtName.setText(arrayList.get(i).getName());
-        txtdescription.setText(arrayList.get(i).getDescription());
-        txtprice.setText("$ "+arrayList.get(i).getPrice());
-        imageitem.setImageResource(arrayList.get(i).getImg());
-
-        view.setOnClickListener(new View.OnClickListener() {
+        System.out.println(listFilter.size());
+            txtName.setText(listFilter.get(i).getName());
+            txtdescription.setText(listFilter.get(i).getDescription());
+            txtprice.setText("$ "+listFilter.get(i).getPrice());
+            imageitem.setImageResource(listFilter.get(i).getImg());
+        convetView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(ctx,String.valueOf(i),Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(ctx,itemDetail.class);
 
-                intent.putExtra("goods",arrayList.get(i));
+                intent.putExtra("goods",listFilter.get(i));
                 ctx.startActivity(intent);
             }
         });
 
-        return  view;
-
-
+        if(positionSelect == i){
+            viewItem.setBackgroundColor(Color.GRAY);
+        } else{
+            viewItem.setBackgroundColor(Color.WHITE);
+        }
+        return  convetView;
     }
+
+
+
+    public void filterDount(String filter){
+
+        listFilter = new ArrayList<>();
+        for(Goods item: arrayList){
+            if(item.getName().toLowerCase().indexOf(filter) != -1){
+                listFilter.add(item);
+            }
+        }        System.out.println(listFilter.size());
+
+        notifyDataSetChanged();
+    }
+
+
+    public void filterPinkDonut(){
+        filterDount("pink");
+    }
+
+    public void filterFloating(){
+        filterDount("floating");
+    }
+
+    public void allDonut(){
+        listFilter = arrayList;
+        notifyDataSetChanged();
+    }
+
+    public void filterByText(Editable text){
+        listFilter = new ArrayList<>();
+        for(Goods item: arrayList){
+            if(item.getName().toLowerCase().indexOf(text.toString()) != -1){
+                listFilter.add(item);
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+    public void setActiveButton(Button buttonActive, Button btn1, Button btn2) {
+        buttonActive.setBackgroundColor(Color.BLUE);
+        buttonActive.setTextColor(Color.WHITE);
+        btn1.setBackgroundColor(Color.parseColor("#D1CBCB"));
+        btn1.setTextColor(Color.BLACK);
+        btn2.setTextColor(Color.BLACK);
+        btn2.setBackgroundColor(Color.parseColor("#D1CBCB"));
+    }
+
 }
--- app/src/main/java/com/example/androi_week05/MainActivity.java
+++ app/src/main/java/com/example/androi_week05/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.androi_week05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lvGoods = findViewById(R.id.lvGoods);
 
+        EditText txtsearch = findViewById(R.id.txtsearch);
+
+        Button btnD = findViewById(R.id.btnDount);
+        Button btnP = findViewById(R.id.btnPink);
+        Button btnF = findViewById(R.id.btnFloat);
+
         arrayList = new ArrayList<>();
         arrayList.add(new Goods("Tasty Donut","Spicy tasty  donut family",10,R.drawable.donut_yellow_1));
         arrayList.add(new Goods("Pink Donut","Spicy tasty donut family",20,R.drawable.tasty_donut_1));
@@ -26,5 +40,50 @@ public class MainActivity extends AppCompatActivity {
         arrayList.add(new Goods("Tasty Donut","Spicy tasty  donut family",10,R.drawable.donut_red_1));
         adt = new CustomGoodApdapter(this, R.layout.item_listview, arrayList);
         lvGoods.setAdapter(adt);
+
+
+
+        txtsearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                adt.filterByText(editable);
+
+            }
+        });
+
+        btnD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adt.allDonut();
+                adt.setActiveButton(btnD,btnF,btnP);
+            }
+        });
+
+
+        btnF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adt.filterFloating();
+                adt.setActiveButton(btnF,btnD,btnP);
+            }
+        });
+
+        btnP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adt.filterPinkDonut();
+                adt.setActiveButton(btnP,btnD,btnF);
+            }
+        });
     }
 }
